37a700275d77fdb99252aa2846fce2cde9121ec1
0Prashant21 <91116780+0Prashant21@users.noreply.github.com>
2023-10-03 19:04:07 +0530

    Added BubbleSort.cpp
    Sorting a given Array Using BubbleSort.

---
+++ C/C++/BubbleSort.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        bool swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j+1]
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = true;
+            }
+        }
+
+        // If no two elements were swapped in the inner loop, the array is already sorted
+        if (!swapped) {
+            break;
+        }
+    }
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original array:" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    bubbleSort(arr, n);
+
+    cout << "Sorted array:" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
